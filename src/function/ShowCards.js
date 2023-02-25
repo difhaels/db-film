6db75c3f0b5f4875b7d27a89de85ca6717d590ab
Agung Saputra <agungsaputradifh@gmail.com>
2023-02-25 17:20:32 +0700

    remove index.css at showCards

--- src/function/ShowCards.js
+++ src/function/ShowCards.js
@@ -1,10 +1,9 @@
-import "../index.css";
 const showCards = (movie) => {
-  return `<div class="card">
+  return `<div class="border-2 border-slate-400 rounded-md p-5">
       <img src=${movie.Poster} class="h-[450px]"/>
-      <div class="text">
-        <h1 class="title">${movie.Title}</h1>
-        <h1 class="year">${movie.Year}</h1>
+      <div class="flex flex-col space-y-1">
+        <h1 class="text-2xl">${movie.Title}</h1>
+        <h1 class="text-slate-600 text-xl">${movie.Year}</h1>
         <button class="show-detail bg-sky-500 hover:bg-sky-800 text-white py-2 text-xl rounded-md w-[172px]" data-imdbid="${movie.imdbID}">Show Details</button>
       </div>
     </div>`;
